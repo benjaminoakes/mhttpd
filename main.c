f1f32acdad1d88d378f3edafb19946533aa5ae7d
Benjamin Oakes <ben@benjaminoakes.com>
2011-10-22 21:58:13 -0400

    "cat" of public/index.html

--- main.c
+++ main.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
 
-main() {
-    puts("Hello, HTTP world!");
+char *progname;
+
+void cat(char *path) {
+    FILE *file;
+    char c;
+
+    file = fopen(path, "r");
+
+    while ((c = fgetc(file)) != EOF) {
+        putchar(c);
+    }
+
+    fclose(file);
+}
+
+int main(int argc, char *argv[]) {
+    char* index;
+
+    progname = argv[0];
+
+    if (argc == 2) {
+        index = argv[1];
+    } else {
+        index = "public/index.html";
+    }
+
+    printf("[%s] starting...\n", progname);
+    printf("[%s] started\n", progname);
+
+    cat(index);
+
+    printf("[%s] finished\n", progname);
+
     return 0;
 }
