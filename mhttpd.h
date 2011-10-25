8081dd1b3625e3a45dd48f48660ff6a9b0de65d2
Benjamin Oakes <ben@benjaminoakes.com>
2011-10-25 07:20:17 -0400

    Fix includes for bzero and exit... I'm not sure why errors weren't coming up before.

--- mhttpd.h
+++ mhttpd.h
@@ -1,6 +1,8 @@
-#include <stdio.h> /* for fopen, etc */
-#include <sys/socket.h> /* for socket */
 #include <netinet/in.h> /* for serv_addr, cli_addr, AF_INET, INADDR_ANY, etc */
+#include <stdio.h> /* for fopen, etc */
+#include <stdlib.h> /* for exit */
+#include <strings.h> /* for bzero */
+#include <sys/socket.h> /* for socket */
 
 #ifndef MTTPD_H
 #define MTTPD_H
