7de521be037ef83d4d894c3c89a9edc98d426a50
Benjamin Oakes <ben@benjaminoakes.com>
2011-10-24 22:09:49 -0400

    Basic opening a socket on a port and sending back a message

--- mhttpd.c
+++ mhttpd.c
@@ -1,7 +1,62 @@
 #include "mhttpd.h"
 
+#define MAX_Q_LEN 5
+
+static void ns_error(const char *msg) {
+    perror("Error opening socket");
+    exit(1);
+}
+
+/*
+ * Begin listening on port
+ * @see http://www.linuxhowtos.org/data/6/server.c, http://www.linuxhowtos.org/C_C++/socket.htm
+ */
 static void ns_listen(int port) {
-    puts("got in");
+    int servsockfd, clisockfd;
+    socklen_t clilen;
+    char buffer[BUFSIZ];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+
+    servsockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (servsockfd < 0) {
+       ns_error("ERROR opening socket");
+    }
+
+    bzero((char*) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(port);
+
+    if (bind(servsockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        ns_error("Error binding");
+    }
+
+    listen(servsockfd, MAX_Q_LEN);
+    clilen = sizeof(cli_addr);
+    clisockfd = accept(servsockfd, (struct sockaddr *) &cli_addr, &clilen);
+
+    if (clisockfd < 0) {
+        ns_error("Error accepting");
+    }
+
+    bzero(buffer, BUFSIZ);
+    n = read(clisockfd, buffer, BUFSIZ - 1);
+
+    if (n < 0) {
+        ns_error("Error reading from socket");
+    }
+
+    printf("%s\n", buffer);
+    n = write(clisockfd, "I got your message\n", 19);
+
+    if (n < 0) {
+        ns_error("Error writing to socket");
+    }
+
+    close(clisockfd);
+    close(servsockfd);
 }
 
 ns const mhttp = {
--- mhttpd.h
+++ mhttpd.h
@@ -1,5 +1,6 @@
-#include <stdio.h>
-#include <sys/socket.h> /* for socket() */
+#include <stdio.h> /* for fopen, etc */
+#include <sys/socket.h> /* for socket */
+#include <netinet/in.h> /* for serv_addr, cli_addr, AF_INET, INADDR_ANY, etc */
 
 #ifndef MTTPD_H
 #define MTTPD_H
