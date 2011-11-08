63968e739790e6c87b4c08c8399671da745f2bc8
Benjamin Oakes <ben@benjaminoakes.com>
2011-11-07 20:24:03 -0500

    Change ifndef guard define

--- mhttpd.h
+++ mhttpd.h
@@ -1,11 +1,12 @@
+#ifndef __mhttpd_h__
+#define __mhttpd_h__
+
 #include <netinet/in.h> /* for serv_addr, cli_addr, AF_INET, INADDR_ANY, etc */
 #include <stdio.h> /* for fopen, etc */
 #include <stdlib.h> /* for exit */
 #include <strings.h> /* for bzero */
 #include <sys/socket.h> /* for socket */
 
-#ifndef MTTPD_H
-#define MTTPD_H
 typedef struct {
     void (* const listen)(int);
 } mhttp_ns;
