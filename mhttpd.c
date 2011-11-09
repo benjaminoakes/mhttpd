a5744929ffe75e4b2e9608f75f2c325a17278565
Benjamin Oakes <ben@benjaminoakes.com>
2011-11-08 20:20:00 -0500

    Simple GET request response, reading content from the filesystem.

--- mhttpd.c
+++ mhttpd.c
@@ -3,19 +3,24 @@
 #define MAX_Q_LEN 5
 
 /**
- * Print out a file, like Unix `cat`.
+ * Get a file, like Unix `cat`.
  */
-void ns_cat(char* path) {
-    FILE *file;
+void ns_cat(char* path, int res_sock_fd) {
     char c;
+    int n;
+    FILE *file;
 
     file = fopen(path, "r");
 
+    // TODO buffer read and write into a char[BUFSIZ]
     while ((c = fgetc(file)) != EOF) {
-        putchar(c);
+        n = write(res_sock_fd, &c, 1);
+        check(n >= 0, "Error writing to socket");
     }
 
+error:
     fclose(file);
+    close(res_sock_fd);
 }
 
 /**
@@ -25,15 +30,14 @@ void ns_cat(char* path) {
  * @see http://www.linuxhowtos.org/data/6/server.c
  */
 static void ns_listen(int port) {
-    int req_sock_fd,
-        res_sock_fd,
-        bind_result,
-        n;
+    int req_sock_fd;
+    int res_sock_fd;
+    int bind_result;
+    int n;
     socklen_t res_len;
     char req_buffer[BUFSIZ];
     struct sockaddr_in req_addr, res_addr;
     char* header = "HTTP/1.0 200 OK\nContent-Type: text/html\n\n"; // TODO Content-Length: 1234
-    char* body = "<!DOCTYPE html>\n<html><head><title>Hello</title></head><body><h1>Hello from mhttpd</h1><p>This is just a test</p></body></html>\n";
 
     req_sock_fd = socket(AF_INET, SOCK_STREAM, 0);
     check(req_sock_fd >= 0, "Error opening socket");
@@ -49,21 +53,18 @@ static void ns_listen(int port) {
     listen(req_sock_fd, MAX_Q_LEN);
     res_len = sizeof(res_addr);
     res_sock_fd = accept(req_sock_fd, (struct sockaddr *) &res_addr, &res_len);
-
     check(res_sock_fd >= 0, "Error accepting");
 
     bzero(req_buffer, BUFSIZ);
     n = read(res_sock_fd, req_buffer, BUFSIZ - 1);
-
     check(n >= 0, "Error reading from socket");
 
     printf("%s", req_buffer);
     printf("%s", header);
     n = write(res_sock_fd, header, strlen(header));
-    printf("%s", body);
-    n = write(res_sock_fd, body, strlen(body));
-
     check(n >= 0, "Error writing to socket");
+
+    ns_cat("public/index.html", res_sock_fd);
 
 error:
     close(res_sock_fd);
